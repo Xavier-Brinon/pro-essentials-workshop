18b358c60c449a1300fb20b3a5fd283ac9799a35
Xavier Brinon <xavier@brinon.eu>
2025-05-24 16:56:47 +0100

    chore: :wrench: update .gitignore and biome.json (#1)

--- app.config.ts
+++ app.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from '@tanstack/react-start/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  tsr: {
-    appDirectory: './src',
-  },
+	tsr: {
+		appDirectory: './src',
+	},
 	vite: {
 		plugins: [
 			tsConfigPaths({
